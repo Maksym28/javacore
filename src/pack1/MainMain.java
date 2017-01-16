34b37f2476b573272756bf418cadec4aedd3eb28
Maksym Lobenko <maksym.lobenko@gmail.com>
2017-01-16 16:45:52 +0200

    initial commit

---
+++ src/pack1/MainMain.java
@@ -0,0 +1,17 @@
+package pack1;
+
+/**
+ * Created by lobenko on 16.01.2017.
+ */
+
+import java.util.Scanner;
+
+
+public class MainMain {
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.println(scan.nextLine());
+
+    }
+}
